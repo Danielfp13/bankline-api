557a74467583493b2435ad53c9b718ad10eb3abe
Daniel Pereira <daniel.si@outlook.com.br>
2022-05-03 18:51:22 -0300

    Configuração para heroku

--- bankline-api/src/main/java/com/dio/santander/bankline/api/controller/MovimentacaoController.java
+++ bankline-api/src/main/java/com/dio/santander/bankline/api/controller/MovimentacaoController.java
@@ -17,7 +17,7 @@ import java.net.URI;
 import java.util.List;
 
 @RestController
-@RequestMapping("/movimentacaoes")
+@RequestMapping("/movimentacoes")
 @AllArgsConstructor
 public class MovimentacaoController {
 
--- bankline-api/src/main/java/com/dio/santander/bankline/api/repository/MovimentacaoRepository.java
+++ bankline-api/src/main/java/com/dio/santander/bankline/api/repository/MovimentacaoRepository.java
@@ -4,4 +4,5 @@ import com.dio.santander.bankline.api.model.Movimentacao;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MovimentacaoRepository extends JpaRepository<Movimentacao, Integer> {
+
 }
